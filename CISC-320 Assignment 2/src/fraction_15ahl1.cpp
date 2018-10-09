1b28db4d6f3da915ca75b6c6e8617ba5087f4f95
The End Game <alastair.lewis10@gmail.com>
2018-10-09 15:59:55 -0400

    Halfway

--- CISC-320 Assignment 2/src/fraction_15ahl1.cpp
+++ CISC-320 Assignment 2/src/fraction_15ahl1.cpp
@@ -1,15 +1,116 @@
-//============================================================================
-// Name        : CISC-320.cpp
-// Author      : Team Bagot
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+ * fraction15ahl1.cpp
+ *
+ *  Created on: Oct 9, 2018
+ *      Author: Alastair
+ */
 
-#include <iostream>
+#include "fraction_15ahl1.h"
+#include <algorithm>
+#include <cstdlib>
+
 using namespace std;
 
-int main() {
-	cout << "Nice" << endl; // prints Nice
-	return 0;
+string FractionException::what() {
+	return "Zero Denominator is Not Allowed.";
 }
+
+Fraction::Fraction() {
+	n = 0;
+	d = 1;
+}
+
+Fraction::Fraction(int a){
+	n = a;
+	d = 1;
+}
+
+Fraction::Fraction(int a, int b){
+	n = a;
+	d = b;
+	if(d == 0)
+		throw FractionException();
+
+	//Handling Negatives
+	if(n < 0 && d < 0){
+		n = -n;
+		d = -d;
+	}
+	else if (n > 0 && d < 0){
+		n = -n;
+		d = -d;
+	}
+
+	//Handling GCD
+	int g = __gcd(abs(n), abs(d));
+	if(g > 1){
+		n /= g;
+		d /= g;
+	}
+}
+
+int Fraction::numerator() const{
+	return n;
+}
+
+int Fraction::denominator() const{
+	return d;
+}
+
+Fraction Fraction::reduce(Fraction f){
+	int g;
+	Fraction out(f.numerator(), f.denominator());
+	while(out.numerator() > out.denominator() && out.denominator() % out.numerator() == 0){
+		g = __gcd(out.numerator(), out.denominator());
+		out.n /= g;
+		out.d /= g;
+	}
+	return out;
+}
+
+Fraction operator+(const Fraction& a, const Fraction& b){
+	int lcm = (a.denominator() * b.denominator()) / __gcd(a.denominator(), b.denominator());
+
+	int n1, n2;
+	n1 = a.numerator() * (lcm/a.denominator());
+	n2 = b.numerator() * (lcm/b.denominator());
+
+	return Fraction((n1 + n2), lcm);
+}
+
+Fraction operator-(const Fraction& a, const Fraction& b){
+	int lcm = (a.denominator() * b.denominator()) / __gcd(a.denominator(), b.denominator());
+
+	int n1, n2;
+	n1 = a.numerator() * (lcm/a.denominator());
+	n2 = b.numerator() * (lcm/b.denominator());
+
+	return Fraction((n1 - n2), lcm);
+}
+
+Fraction operator*(const Fraction& a, const Fraction& b){
+	int n = a.numerator() * b.numerator();
+	int d = a.denominator() * b.denominator();
+
+	Fraction out(n, d);
+	return out.reduce(out);
+}
+
+Fraction operator/(const Fraction& a, const Fraction& b){
+	Fraction out(a.numerator() * b.denominator(), a.denominator() * b.numerator());
+	return out.reduce(out);
+}
+
+Fraction operator-(const Fraction& a){
+	return Fraction(-a.numerator(), a.denominator());
+}
+
+ostream& operator<<(ostream& out, const Fraction& f){
+	out << f.numerator() << "/" << f.denominator();
+	return out;
+}
+
+
+
+
+
---
+++ CISC-320 Assignment 2/src/fraction_15ahl1.h
@@ -0,0 +1,43 @@
+/*
+ * fraction15ahl1.h
+ *
+ *  Created on: Oct 9, 2018
+ *      Author: Alastair
+ */
+
+#ifndef FRACTION_15AHL1_H_
+#define FRACTION_15AHL1_H_
+#include <iostream>
+
+using namespace std;
+
+class FractionException {
+public:
+	string what();
+};
+
+class Fraction {
+public:
+	Fraction();
+	Fraction(int a);
+	Fraction(int a, int b);
+	int numerator() const;
+	int denominator() const;
+	Fraction reduce(Fraction f);
+
+private:
+	int n;
+	int d;
+};
+
+Fraction operator+(const Fraction& a, const Fraction& b);
+Fraction operator-(const Fraction& a, const Fraction& b);
+Fraction operator*(const Fraction& a, const Fraction& b);
+Fraction operator/(const Fraction& a, const Fraction& b);
+Fraction operator-(const Fraction& a);
+
+ostream& operator<<(ostream& out, const Fraction& f);
+
+
+
+#endif
