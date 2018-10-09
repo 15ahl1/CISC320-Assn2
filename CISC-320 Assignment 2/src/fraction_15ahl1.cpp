1e1a7fd49a8b9e1fa1e0c14ba0e57f7e17c02761
The End Game <alastair.lewis10@gmail.com>
2018-10-09 12:15:27 -0400

    Initial

---
+++ CISC-320 Assignment 2/src/fraction_15ahl1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CISC-320.cpp
+// Author      : Team Bagot
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nice" << endl; // prints Nice
+	return 0;
+}
